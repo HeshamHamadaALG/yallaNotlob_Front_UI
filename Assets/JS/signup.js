e0ad0ae068f6be16e95ebb0bafa07a36e7e0957d
HeshamHamadaALG <heshamhamada222@gmail.com>
2019-03-24 15:47:14 +0200

    user Login

--- Assets/JS/signup.js
+++ Assets/JS/signup.js
@@ -1,6 +1,7 @@
 $('document').ready(function () {
 
     $('#submit_btn').click(function (e) {
+        $('#al').remove();
         e.preventDefault();
         var data = {};
         data.name = $('#username').val();
@@ -16,32 +17,32 @@ $('document').ready(function () {
         // start Validation 
         
         if (data.name == '' || data.email == '' || data.password == '' || conPass == '') {
-            $('#al').remove();
+            // $('#al').remove();
             $('#alert').append("<div id=\"al\" class=\"alert\"><span> Please Fill All Fields</span><div>");
         } else if (data.password != conPass) {
-            $('#al').remove();
+            // $('#al').remove();
             $('#alert').append("<div id=\"al\" class=\"alert\"><span> Make Sure to Confirm Password</span><div>");
         } else if (!data.email.match(mailformat)) {
-            $('#al').remove();
+            // $('#al').remove();
             $('#alert').append("<div id=\"al\" class=\"alertBlue\"><span>Please Enter a valid E-mail !!</span><div>");
         } else if (data.name.length < 4) {
-            $('#al').remove();
+            // $('#al').remove();
             $('#alert').append("<div id=\"al\" class=\"alertYellow\"><span>Please Enter a valid Name (More than 3 Charachters) !</span><div>");
         } else if (data.password.length < 8) {
-            $('#al').remove();
+            // $('#al').remove();
             $('#alert').append("<div id=\"al\" class=\"alertYellow\"><span>Make Sure That password more than 8 Charachters !</span><div>");
         } else {
-            $('#al').remove();
+            // $('#al').remove();
 
             jQuery.ajax({
                 type: "POST",
                 url: "http://localhost:3000/users",
                 data: dataJSON,
-                // dataType: "application/json",
                 contentType: "application/json",
                 cache: false,
                 success: function (response) {
                     console.log(response)
+                    window.location.href = "./login.html";
                 }
 
             });
